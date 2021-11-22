415d9aeb8fbd63ef85ed62cfb3b4569145d7248a
lucass2601 <lucas.fischer2002@t-online.de>
2021-11-22 16:52:01 +0100

    methoden auslagerung

--- app.js
+++ app.js
@@ -3,17 +3,10 @@ const app = express();
 
 const port = 3000
 
-const pool = require('./db') // Datenbank Objekt wird importiert
+let students = require('./routes/students')
 
-app.get('/students', async (req, res) => {
-    try {
-        const allStudents = await pool.query
-            ("SELECT student_id, first_name, last_name, title FROM students INNER JOIN classes ON classes.class_id = students.class_fid")
-        res.json(allStudents.rows)
-    } catch (err) {
-        console.log(err.message)
-    }
-})
+app.get('/students', students.getStudents)
+app.get('/students/:student_id', students.getStudentById)
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
