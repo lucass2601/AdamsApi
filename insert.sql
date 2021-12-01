db01ead14579a5e37e02fa11efa7eda1f21d43f7
lucass2601 <lucas.fischer2002@t-online.de>
2021-12-01 09:05:45 +0100

    added insert script

---
+++ insert.sql
@@ -0,0 +1,12 @@
+INSERT INTO classes VALUES (DEFAULT, 'ita3a');
+
+INSERT INTO students VALUES (DEFAULT, 'Lucas', 'Fischer', 'lufischer', 'yota95', 1);
+INSERT INTO students VALUES (DEFAULT, 'Jonathan', 'Gekkel', 'jogekkel', 'tito81', 1);
+INSERT INTO students VALUES (DEFAULT, 'Julian', 'Gesang', 'jugesang', 'zifa52', 1);
+INSERT INTO students VALUES (DEFAULT, 'Emre', 'Il', 'emil', 'miri70', 1);
+INSERT INTO students VALUES (DEFAULT, 'Pascal', 'Moritz', 'pamoritz', 'gasu38', 1);
+INSERT INTO students VALUES (DEFAULT, 'Hamid', 'Naser', 'hanaser', 'zehe65', 1);
+INSERT INTO students VALUES (DEFAULT, 'Christoph', 'Reimann', 'chreimann', 'zosa28', 1);
+INSERT INTO students VALUES (DEFAULT, 'Joel', 'Riekemann', 'joriekemann', 'ruje49', 1);
+INSERT INTO students VALUES (DEFAULT, 'Kevin', 'Riexinger', 'keriexinger', 'kifa69', 1);
+INSERT INTO students VALUES (DEFAULT, 'Nate', 'Steinberg', 'nasteinberg', 'xofa65', 1);
